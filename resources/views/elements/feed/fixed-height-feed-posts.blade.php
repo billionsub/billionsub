91c08ec2d503dfbde59b17bc6b62fab94b7985dc
billionsub <info@billionsub.com>
2025-11-03 14:13:31 +0400

    Delete resources directory

--- resources/views/elements/feed/fixed-height-feed-posts.blade.php
+++
@@ -1,3 +0,0 @@
-<style>
-    {!! str_replace('$height', $height, file_get_contents(asset('/css/posts/height-locked-posts.css'))) !!}
-</style>
--- resources/views/elements/translations.blade.php
+++
@@ -1,3 +0,0 @@
-<script>
-    window.translations = {!! Session::get('app_translations') ? Session::get('app_translations') : '[]' !!};
-</script>
